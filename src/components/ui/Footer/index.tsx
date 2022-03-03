1de622dd1f20f125a222422a75e45b5453c67dc5
kotaro1574 <shake1574@gmail.com>
2022-03-04 07:28:00 +0900

    create: Footer作成

---
+++ src/components/ui/Footer/index.tsx
@@ -0,0 +1,33 @@
+import {
+  AspectRatio,
+  Box,
+  Center,
+  Grid,
+  Image,
+  ListItem,
+  UnorderedList,
+} from '@chakra-ui/react'
+import { staticPath } from '@src/lib/$path'
+
+export const Footer = () => {
+  return (
+    <Grid
+      bg={'#FAF7ED'}
+      h={'500px'}
+      mt={'auto'}
+      templateColumns={'repeat(2,1fr)'}
+    >
+      <Center>
+        <UnorderedList>
+          <ListItem>Lorem ipsum dolor sit amet</ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </UnorderedList>
+      </Center>
+      <AspectRatio>
+        <Image src={staticPath.images.footerMap_svg} />
+      </AspectRatio>
+    </Grid>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@src/components/ui/Header'
 import type { NextPage } from 'next'
+import { Footer } from '@src/components/ui/Footer'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
       <div>けろ</div>
+      <Footer />
     </>
   )
 }
