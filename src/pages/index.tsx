98b75d69990529a4631462b4b4f92a3e2d878d81
kotaro1574 <shake1574@gmail.com>
2022-03-05 22:31:18 +0900

    fix: lintエラー解消

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
+import { Text } from '@chakra-ui/react'
 import { Footer } from '@src/components/ui/Footer'
 import { Header } from '@src/components/ui/Header'
 import type { NextPageWithLayout } from 'next'
-import { Text } from '@chakra-ui/react'
 
 const Home: NextPageWithLayout = () => {
   return <Text>こたろ！！！！</Text>
