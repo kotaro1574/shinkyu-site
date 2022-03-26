c2e423ed8a911f0a3f2302782510ba0e756089d2
kotaro1574 <shake1574@gmail.com>
2022-03-26 17:06:33 +0900

    create: TelephoneIcon

---
+++ src/components/ui/Icon/TelephoneIcon/index.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, IconProps } from '@src/components/ui/Icon/BaseIcon'
+import { BsFillTelephoneFill } from 'react-icons/bs'
+
+export const TelephoneIcon = (props: IconProps) => {
+  return <BaseIcon icon={BsFillTelephoneFill} {...props} />
+}
--- src/components/ui/Navigation/GlobalNavigation/index.tsx
+++ src/components/ui/Navigation/GlobalNavigation/index.tsx
@@ -1,10 +1,16 @@
 import { Button, Grid, GridProps } from '@chakra-ui/react'
 import { LineIcon } from '@src/components/ui/Icon/LineIcon'
+import { TelephoneIcon } from '@src/components/ui/Icon/TelephoneIcon'
 
 export const GlobalNavigation = ({ ...gridProps }: GridProps) => {
   return (
     <Grid as={'nav'} templateColumns={'repeat(2, 1fr)'} {...gridProps}>
-      <Button bg={'#DBB025'} color={'white'} rounded={0}>
+      <Button
+        bg={'#DBB025'}
+        color={'white'}
+        leftIcon={<TelephoneIcon />}
+        rounded={0}
+      >
         電話で予約
       </Button>
       <Button
