9a7dd77dbca4561b67b331ad39b6fc46efd7a1a5
kotaro1574 <shake1574@gmail.com>
2022-03-26 15:34:49 +0900

    create: LayoutWithStickyHeaderAndMenuコンポーネント作成

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,4 @@
-import { Box, Button, Grid } from '@chakra-ui/react'
-import { Content } from '@src/components/layouts/Content'
-import { Header } from '@src/components/ui/Header'
+import { LayoutWithStickyHeaderAndMenu } from '@src/components/layouts/LayoutWithStickyHeaderAndMenu'
 import { HomeContent } from '@src/pages/content'
 import type { NextPageWithLayout } from 'next'
 
@@ -9,26 +7,7 @@ const Home: NextPageWithLayout = () => {
 }
 
 Home.getLayout = (page) => {
-  return (
-    <Box maxW={'540px'}>
-      <Header position={'sticky'} top={0} zIndex={'sticky'} />
-      <Content>{page}</Content>
-      <Grid
-        bottom={0}
-        maxW={'540px'}
-        pos={'sticky'}
-        templateColumns={'repeat(2, 1fr)'}
-        w={'full'}
-      >
-        <Button bg={'#DBB025'} color={'white'} rounded={0} w={'full'}>
-          電話で予約
-        </Button>
-        <Button bg={'green.500'} color={'white'} rounded={0} w={'full'}>
-          Lineで予約
-        </Button>
-      </Grid>
-    </Box>
-  )
+  return <LayoutWithStickyHeaderAndMenu>{page}</LayoutWithStickyHeaderAndMenu>
 }
 
 export default Home
