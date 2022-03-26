0a0bc9693669cf2950759ca164dc93a6dd8b9ec8
kotaro1574 <shake1574@gmail.com>
2022-03-26 16:53:13 +0900

    create: LineIcon

---
+++ src/components/ui/Icon/LineIcon/index.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, IconProps } from '@src/components/ui/Icon/BaseIcon'
+import { BsLine } from 'react-icons/bs'
+
+export const LineIcon = (props: IconProps) => {
+  return <BaseIcon icon={BsLine} {...props} />
+}
--- src/components/ui/Navigation/GlobalNavigation/index.tsx
+++ src/components/ui/Navigation/GlobalNavigation/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Grid, GridProps } from '@chakra-ui/react'
+import { LineIcon } from '@src/components/ui/Icon/LineIcon'
 
 export const GlobalNavigation = ({ ...gridProps }: GridProps) => {
   return (
@@ -6,7 +7,12 @@ export const GlobalNavigation = ({ ...gridProps }: GridProps) => {
       <Button bg={'#DBB025'} color={'white'} rounded={0}>
         電話で予約
       </Button>
-      <Button bg={'green.500'} color={'white'} rounded={0}>
+      <Button
+        bg={'green.500'}
+        color={'white'}
+        leftIcon={<LineIcon />}
+        rounded={0}
+      >
         Lineで予約
       </Button>
     </Grid>
