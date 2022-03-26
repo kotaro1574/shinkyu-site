9ea505be791dac9596f1e2db6ad9349018292749
kotaro1574 <shake1574@gmail.com>
2022-03-26 15:34:35 +0900

    create: GlobalNavigationコンポーネント作成

---
+++ src/components/ui/Navigation/GlobalNavigation/index.tsx
@@ -0,0 +1,14 @@
+import { Button, Grid, GridProps } from '@chakra-ui/react'
+
+export const GlobalNavigation = ({ ...gridProps }: GridProps) => {
+  return (
+    <Grid as={'nav'} templateColumns={'repeat(2, 1fr)'} {...gridProps}>
+      <Button bg={'#DBB025'} color={'white'} rounded={0}>
+        電話で予約
+      </Button>
+      <Button bg={'green.500'} color={'white'} rounded={0}>
+        Lineで予約
+      </Button>
+    </Grid>
+  )
+}
