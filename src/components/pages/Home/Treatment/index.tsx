0d8ac031a6374c2a5ed388ee2ca25ebbbc458d5a
kotaro1574 <shake1574@gmail.com>
2022-03-28 13:20:00 +0900

    create: TailorMadeTreatment

--- src/components/pages/Home/Treatment/index.tsx
+++ src/components/pages/Home/Treatment/index.tsx
@@ -1,13 +1,5 @@
-import {
-  AspectRatio,
-  Box,
-  BoxProps,
-  Divider,
-  Flex,
-  Grid,
-  Heading,
-  Text,
-} from '@chakra-ui/react'
+import { AspectRatio, Box, BoxProps, Grid, Text } from '@chakra-ui/react'
+import { TailorMadeTreatment } from '@src/components/pages/Home/Treatment/TailorMadeTreatment'
 import { SubTitle } from '@src/components/ui/Heading/SubTitle'
 import { Title } from '@src/components/ui/Heading/Title'
 import { Image } from '@src/components/ui/Image'
@@ -17,35 +9,7 @@ export const Treatment = (props: BoxProps) => {
   return (
     <Box mt={10} textAlign={'center'} {...props}>
       <Title>当院の施術</Title>
-      <Box mt={4}>
-        <AspectRatio
-          mx={'auto'}
-          overflow={'hidden'}
-          ratio={16 / 8}
-          rounded={'sm'}
-        >
-          <Image
-            alt={'top_image'}
-            layout={'fill'}
-            src={staticPath.images.section1_1_png}
-          />
-        </AspectRatio>
-        <SubTitle mt={2}>一人一人に合わせたオーダーメイドの治療</SubTitle>
-        <Text fontSize={'sm'} mt={2}>
-          患者様の症状や全身の状態をしっかり問診・確認させていただき、患者様一人一人に合わせた施術を行います。
-          <br />
-          施術方法によって追加料金などは発生致しませんのでご安心ください。（価格は税込みとなります。）
-        </Text>
-        <Divider borderColor={'test.900'} mb={2} mt={4} />
-        <Flex alignItems={'flex-end'} justify={'space-between'}>
-          <Box fontSize={'sm'}>治療 (60分程度)</Box>
-          <Flex alignItems={'flex-end'}>
-            <Heading as={'h4'}>4,000円</Heading>
-            <Text fontSize={'xxs'}>（税込み）</Text>
-          </Flex>
-        </Flex>
-        <Divider borderColor={'test.900'} mb={4} mt={2} />
-      </Box>
+      <TailorMadeTreatment mt={4} />
       <Box mt={6}>
         <SubTitle>施術内容一覧</SubTitle>
         <Text fontSize={'sm'} mt={4}>
