f05fba1b407af9ca00df69a1eeefdc2adb6bc775
kotaro1574 <shake1574@gmail.com>
2022-03-28 13:24:31 +0900

    create: TreatmentContents

--- src/components/pages/Home/Treatment/index.tsx
+++ src/components/pages/Home/Treatment/index.tsx
@@ -1,37 +1,14 @@
-import { AspectRatio, Box, BoxProps, Grid, Text } from '@chakra-ui/react'
+import { Box, BoxProps } from '@chakra-ui/react'
 import { TailorMadeTreatment } from '@src/components/pages/Home/Treatment/TailorMadeTreatment'
-import { SubTitle } from '@src/components/ui/Heading/SubTitle'
+import { TreatmentContents } from '@src/components/pages/Home/Treatment/TreatmentContents'
 import { Title } from '@src/components/ui/Heading/Title'
-import { Image } from '@src/components/ui/Image'
-import { staticPath } from '@src/lib/$path'
 
 export const Treatment = (props: BoxProps) => {
   return (
     <Box mt={10} textAlign={'center'} {...props}>
       <Title>当院の施術</Title>
-      <TailorMadeTreatment mt={4} />
-      <Box mt={6}>
-        <SubTitle>施術内容一覧</SubTitle>
-        <Text fontSize={'sm'} mt={4}>
-          症状や現在の状態・ご希望を来院時にしっかり問診させていただき、患者様に最適なプランを提案させていただきます。
-        </Text>
-        <Grid gap={4} mt={4} templateColumns={'repeat(2, 1fr)'}>
-          {Array.from({ length: 5 }).map((item, index) => (
-            <AspectRatio
-              key={index}
-              overflow={'hidden'}
-              ratio={150 / 120}
-              rounded={'sm'}
-            >
-              <Image
-                alt={'top_image'}
-                layout={'fill'}
-                src={staticPath.images.top1_jpg}
-              />
-            </AspectRatio>
-          ))}
-        </Grid>
-      </Box>
+      <TailorMadeTreatment mt={6} />
+      <TreatmentContents mt={6} />
     </Box>
   )
 }
