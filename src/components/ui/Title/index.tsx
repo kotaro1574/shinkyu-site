82ce7f8de189da4df7f076da7cdc2e2a1a043ca9
kotaro1574 <shake1574@gmail.com>
2022-03-28 12:28:49 +0900

    update: Titleタグ修正

--- src/components/ui/Title/index.tsx
+++ src/components/ui/Title/index.tsx
@@ -1,14 +1,15 @@
 import { Divider, Grid, Heading } from '@chakra-ui/react'
+import { ReactNode } from 'react'
 
 type Props = {
-  title: string
+  children: ReactNode
 }
 
-export const Title = ({ title }: Props) => {
+export const Title = ({ children }: Props) => {
   return (
     <Grid gap={2} m={'auto'} placeItems={'center'}>
       <Heading fontSize={'xl'} fontWeight={'medium'}>
-        {title}
+        {children}
       </Heading>
       <Divider borderBottomWidth={2} borderColor={'#E7C65B'} w={'110px'} />
     </Grid>
--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -15,26 +15,26 @@ import { Image } from '@src/components/ui/Image'
 import { Title } from '@src/components/ui/Title'
 import { staticPath } from '@src/lib/$path'
 
-const FEATURES_OF_HOSPITAL = [
-  {
-    content:
-      '治療を行う前に、問診、全身の状態を確認していき患者様ひとりひとりの身体にあった施術を提供させていただきます。',
-    image: staticPath.images.section1_1_png,
-    title: '一人一人に合った施術',
-  },
-  {
-    content:
-      '当院の鍼灸治療で使用する針は すべて使い捨てです。 感染症の心配もありません。',
-    image: staticPath.images.section1_2_JPG,
-    title: '安心安全な施術',
-  },
-  {
-    content:
-      '当院は完全予約制です。 プラベートな空間でご症状について 安心してご相談ください。',
-    image: staticPath.images.secttion1_3_JPG,
-    title: 'プライベートな空間で',
-  },
-]
+// const FEATURES_OF_HOSPITAL = [
+//   {
+//     content:
+//       '治療を行う前に、問診、全身の状態を確認していき患者様ひとりひとりの身体にあった施術を提供させていただきます。',
+//     image: staticPath.images.section1_1_png,
+//     title: '一人一人に合った施術',
+//   },
+//   {
+//     content:
+//       '当院の鍼灸治療で使用する針は すべて使い捨てです。 感染症の心配もありません。',
+//     image: staticPath.images.section1_2_JPG,
+//     title: '安心安全な施術',
+//   },
+//   {
+//     content:
+//       '当院は完全予約制です。 プラベートな空間でご症状について 安心してご相談ください。',
+//     image: staticPath.images.secttion1_3_JPG,
+//     title: 'プライベートな空間で',
+//   },
+// ]
 
 export const HomeContent = () => {
   return (
@@ -61,11 +61,10 @@ export const HomeContent = () => {
           >
             詳しくはこちら
           </Button>
-
           <BeautyAcupunctureCard />
         </Grid>
         <Box mt={10} textAlign={'center'}>
-          <Title title={'当院の施術'} />
+          <Title>当院の施術</Title>
           <Box mt={4}>
             <AspectRatio
               mx={'auto'}
