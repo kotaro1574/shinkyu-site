4ffb11ae642d5bfca430f7a11901e6de19013e61
kotaro1574 <shake1574@gmail.com>
2022-03-28 19:52:31 +0900

    update: 不要Props削除

--- src/components/pages/About/FeaturesOfHospital/index.tsx
+++ src/components/pages/About/FeaturesOfHospital/index.tsx
@@ -1,17 +1,40 @@
 import { Box, BoxProps } from '@chakra-ui/react'
 import { FeaturesOfHospitalItem } from '@src/components/pages/About/FeaturesOfHospital/FeaturesOfHospitalItem'
 import { Title } from '@src/components/ui/Heading/Title'
-import { FeatureOfHospital } from '@src/pages/about/content'
+import { staticPath } from '@src/lib/$path'
 
-type Props = {
-  feature: FeatureOfHospital[]
-} & BoxProps
+type FeatureOfHospital = {
+  content: string
+  image: string
+  title: string
+}
 
-export const FeaturesOfHospital = ({ feature, ...boxProps }: Props) => {
+const FEATURES_OF_HOSPITAL: FeatureOfHospital[] = [
+  {
+    content:
+      '治療を行う前に、問診、全身の状態を確認していき患者様ひとりひとりの身体にあった施術を提供させていただきます。',
+    image: staticPath.images.section1_1_png,
+    title: '一人一人に合った施術',
+  },
+  {
+    content:
+      '当院の鍼灸治療で使用する針は すべて使い捨てです。 感染症の心配もありません。',
+    image: staticPath.images.section1_2_JPG,
+    title: '安心安全な施術',
+  },
+  {
+    content:
+      '当院は完全予約制です。 プラベートな空間でご症状について 安心してご相談ください。',
+    image: staticPath.images.secttion1_3_JPG,
+    title: 'プライベートな空間で',
+  },
+]
+
+export const FeaturesOfHospital = (props: BoxProps) => {
   return (
-    <Box {...boxProps}>
+    <Box {...props}>
       <Title>当院の特徴</Title>
-      {feature.map((item, index) => (
+      {FEATURES_OF_HOSPITAL.map((item, index) => (
         <FeaturesOfHospitalItem {...item} key={index} mt={6} />
       ))}
     </Box>
