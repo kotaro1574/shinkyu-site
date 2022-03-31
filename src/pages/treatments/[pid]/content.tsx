a92d61b67ac809ad0e8358e35ce93c4bd4ea749f
kotaro1574 <shake1574@gmail.com>
2022-03-31 22:47:30 +0900

    update: FeaturesOfHospitalItem →　ContentItemに変更

--- src/pages/treatments/[pid]/content.tsx
+++ src/pages/treatments/[pid]/content.tsx
@@ -1,12 +1,23 @@
-import { AspectRatio, Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { ContentItem } from '@src/components/ui/Content/ContentItem'
 import { Title } from '@src/components/ui/Heading/Title'
-import { Image } from '@src/components/ui/Image'
 import { staticPath } from '@src/lib/$path'
 
 export const TreatmentContent = () => {
   return (
-    <Box p={4}>
+    <Box p={4} pt={10}>
       <Title>鍼灸</Title>
+      <ContentItem
+        content={
+          '鍼灸は一般に｢はり・きゅう｣または｢しんきゅう｣と呼ばれ、疾患や症状に適した経穴(ツボ)に金属の細い針を刺入したり艾(もぐさ)を置いて燃焼させたりなど、生体に刺激を加えることで元々身体に備わっている病気を治す力を高めて元気にする治療法です。'
+        }
+        image={
+          staticPath.images.Treatment.Detail.AcupunctureAndMoxibustion
+            .Acupuncture_jpeg
+        }
+        mt={10}
+        title={'針'}
+      />
     </Box>
   )
 }
