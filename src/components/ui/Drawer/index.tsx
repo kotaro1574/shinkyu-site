430239852b2c411ac6d962ff061df3b416f1be85
kotaro1574 <shake1574@gmail.com>
2022-04-02 12:43:09 +0900

    update: HeaderStyle調整

--- src/components/ui/Drawer/index.tsx
+++ src/components/ui/Drawer/index.tsx
@@ -1,4 +1,5 @@
 import {
+  AspectRatio,
   Box,
   Drawer as BaseDrawer,
   DrawerBody,
@@ -7,6 +8,9 @@ import {
   DrawerHeader,
   DrawerOverlay,
 } from '@chakra-ui/react'
+import { Image } from '@src/components/ui/Image'
+import { pagesPath, staticPath } from '@src/lib/$path'
+import { useRouter } from 'next/router'
 
 type Props = {
   isOpen: boolean
@@ -14,14 +18,44 @@ type Props = {
 }
 
 export const Drawer = ({ isOpen, onClose }: Props) => {
+  const router = useRouter()
   return (
     <BaseDrawer isOpen={isOpen} placement={'left'} onClose={onClose}>
       <DrawerOverlay>
-        <DrawerContent>
+        <DrawerContent roundedRight={'md'}>
           <DrawerCloseButton />
-          <DrawerHeader>Menu</DrawerHeader>
-          <DrawerBody>
-            <Box>こたろ</Box>
+          <DrawerHeader bg={'test.900'} roundedTopRight={'md'}>
+            <AspectRatio
+              cursor={'pointer'}
+              ratio={200 / 20}
+              width={'200px'}
+              onClick={() => router.push(pagesPath.$url())}
+            >
+              <Image
+                alt={'ヘッダーロゴ'}
+                layout={'fill'}
+                src={staticPath.images.footerLog_svg}
+              />
+            </AspectRatio>
+          </DrawerHeader>
+          <DrawerBody p={0}>
+            <Box
+              borderBottomWidth={1}
+              borderColor={'line.dark'}
+              color={'text.primary'}
+              p={4}
+              onClick={() => router.push(pagesPath.about.$url())}
+            >
+              当医院について
+            </Box>
+            <Box
+              borderBottomWidth={1}
+              borderColor={'line.dark'}
+              color={'text.primary'}
+              p={4}
+            >
+              当医院について
+            </Box>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
