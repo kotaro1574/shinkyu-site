19fc31b8e6fc2a191df39a3a41001a9f7d0a2ce8
kotaro1574 <shake1574@gmail.com>
2022-04-07 21:28:51 +0900

    create: ３つアイコン作った

---
+++ src/components/ui/Icon/HospitalLineIcon/index.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, IconProps } from '@src/components/ui/Icon/BaseIcon'
+import { RiHospitalLine } from 'react-icons/ri'
+
+export const HospitalLine = (props: IconProps) => {
+  return <BaseIcon icon={RiHospitalLine} {...props} />
+}
---
+++ src/components/ui/Icon/PinOutlineIcon/index.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, IconProps } from '@src/components/ui/Icon/BaseIcon'
+import { IoPinOutline } from 'react-icons/io5'
+
+export const PinOutlineIcon = (props: IconProps) => {
+  return <BaseIcon icon={IoPinOutline} {...props} />
+}
