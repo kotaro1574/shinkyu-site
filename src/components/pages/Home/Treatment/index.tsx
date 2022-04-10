48c7d43d7420822e4c03b4ef4b6e0b24204b14cc
kotaro1574 <shake1574@gmail.com>
2022-04-10 15:16:36 +0900

    update: 施術内容 → 施術料金

--- src/components/pages/Home/Treatment/index.tsx
+++ src/components/pages/Home/Treatment/index.tsx
@@ -1,14 +1,13 @@
 import { Box, BoxProps } from '@chakra-ui/react'
 import { TailorMadeTreatment } from '@src/components/pages/Home/Treatment/TailorMadeTreatment'
-import { TreatmentContents } from '@src/components/pages/Home/Treatment/TreatmentContents'
 import { Title } from '@src/components/ui/Heading/Title'
 
 export const Treatment = (props: BoxProps) => {
   return (
-    <Box textAlign={'center'} {...props}>
-      <Title>施術内容</Title>
+    <Box {...props}>
+      <Title>施術料金</Title>
       <TailorMadeTreatment mt={6} />
-      <TreatmentContents mt={6} />
+      {/*<TreatmentContents mt={6} />*/}
     </Box>
   )
 }
