59c6820e8f710b35a10ff7fa4bbd753862b7f4b7
kotaro1574 <shake1574@gmail.com>
2022-05-22 14:50:19 +0900

    update: themeカラーをgreenに変更

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,54 +1,48 @@
 import { HamburgerIcon } from '@chakra-ui/icons'
-import { Center, Flex, FlexProps, useDisclosure } from '@chakra-ui/react'
+import {
+  AspectRatio,
+  Center,
+  Flex,
+  FlexProps,
+  Spacer,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { Drawer } from '@src/components/ui/Drawer'
+import { Image } from '@src/components/ui/Image'
+import { pagesPath, staticPath } from '@src/lib/$path'
+import { useRouter } from 'next/router'
 
 export const Header = ({ ...flexProps }: FlexProps) => {
   const { isOpen, onClose, onOpen } = useDisclosure()
+  const router = useRouter()
+
   return (
     <>
       <Flex
         alignItems={'center'}
-        bg={'test.900'}
+        bg={'#fff'}
         h={'50px'}
-        justifyContent={'space-between'}
         maxW={'540px'}
         px={4}
         w={'full'}
         {...flexProps}
       >
-        <Center color={'text.primay'} p={0} rounded={'sm'} onClick={onOpen}>
-          <HamburgerIcon />
-        </Center>
-<<<<<<< HEAD
         <AspectRatio
           cursor={'pointer'}
-          ratio={200 / 20}
-          height={'55px'}
-          width={'422px'}
+          ratio={200 / 40}
+          width={'200px'}
           onClick={() => router.push(pagesPath.$url())}
         >
           <Image
             alt={'ヘッダーロゴ'}
             layout={'fill'}
-            src={staticPath.natsumi.logo.logo14_svg}
+            src={staticPath.natsumi.logo.logo_svg}
           />
         </AspectRatio>
-        <Box />
-=======
-        {/*<AspectRatio*/}
-        {/*  cursor={'pointer'}*/}
-        {/*  ratio={200 / 20}*/}
-        {/*  width={'200px'}*/}
-        {/*  onClick={() => router.push(pagesPath.$url())}*/}
-        {/*>*/}
-        {/*  <Image*/}
-        {/*    alt={'ヘッダーロゴ'}*/}
-        {/*    layout={'fill'}*/}
-        {/*    src={staticPath.images.footerLog_svg}*/}
-        {/*  />*/}
-        {/*</AspectRatio>*/}
-        {/*<Box />*/}
->>>>>>> 4bc320aab37c232a09908aff551251e9a942b555
+        <Spacer />
+        <Center color={'text.primay'} p={0} rounded={'sm'} onClick={onOpen}>
+          <HamburgerIcon />
+        </Center>
       </Flex>
       <Drawer isOpen={isOpen} onClose={onClose} />
     </>
