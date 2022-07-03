1abb7a16df148c200d889db4a33252869d30c119
kotaro1574 <shake1574@gmail.com>
2022-07-03 15:33:37 +0900

    add: recoil導入

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,15 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ApplicationLayout } from '@src/components/layouts/ApplicationLayout'
 import { theme } from '@src/lib/chakra/theme'
 import type { AppPropsWithLayout } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
-    <ChakraProvider theme={theme}>
-      <ApplicationLayout>
-        {getLayout(<Component {...pageProps} />, pageProps)}
-      </ApplicationLayout>
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <ApplicationLayout>
+          {getLayout(<Component {...pageProps} />, pageProps)}
+        </ApplicationLayout>
+      </ChakraProvider>
+    </RecoilRoot>
   )
 }
 
