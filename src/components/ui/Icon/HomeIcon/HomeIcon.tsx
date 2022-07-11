833d8b9c2840f9bb4ba788b4ca94484e0abb49a7
kotaro1574 <shake1574@gmail.com>
2022-07-11 22:57:24 +0900

    add: homeIcon追加

---
+++ src/components/ui/Icon/HomeIcon/HomeIcon.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, IconProps } from '@src/components/ui/Icon/BaseIcon'
+import { AiTwotoneHome } from 'react-icons/ai'
+
+export const HomeIcon = (props: IconProps) => {
+  return <BaseIcon icon={AiTwotoneHome} {...props} />
+}
