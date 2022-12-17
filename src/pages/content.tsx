41d402ac7aa0bbe001787738547cb73ca10dd5e7
kotaro1574 <shake1574@gmail.com>
2022-12-17 21:04:06 +0900

    create: Constantに施術詳細情報の配列を定義

--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -1,6 +1,7 @@
 import { AspectRatio, Box } from '@chakra-ui/react'
 import { AboutSection } from '@src/components/page/home/AboutSection/AboutSection'
 import { IntroductionSection } from '@src/components/page/home/IntroductionSection/IntroductionSection'
+import { TreatmentSection } from '@src/components/page/home/TreatmentSection/TreatmentSection'
 import { Image } from '@src/components/ui/Image'
 
 export const HomeContent = () => {
@@ -15,6 +16,7 @@ export const HomeContent = () => {
       </AspectRatio>
       <AboutSection />
       <IntroductionSection />
+      <TreatmentSection />
     </Box>
   )
 }
