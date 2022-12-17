129f67e37ff09cd56c4a3bb8e33803dccb29afcd
kotaro1574 <shake1574@gmail.com>
2022-12-17 14:13:04 +0900

    update: メインビジュアル配置してみた

--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -1,10 +1,18 @@
-import { Box } from '@chakra-ui/react'
+import { AspectRatio, Box } from '@chakra-ui/react'
 import { Header } from '@src/components/layouts/Header/Header'
+import { Image } from '@src/components/ui/Image'
 
 export const HomeContent = () => {
   return (
     <Box>
       <Header />
+      <AspectRatio h={'700px'} ratio={16 / 9}>
+        <Image
+          alt={'top_image'}
+          layout={'fill'}
+          src={'https://source.unsplash.com/random'}
+        />
+      </AspectRatio>
     </Box>
   )
 }
