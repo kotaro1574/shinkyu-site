03ac8857efed4aff392db08af9d9b5f4855c2386
kotaro1574 <shake1574@gmail.com>
2022-12-18 14:39:06 +0900

    create: 質問内容を表示するコンポーネント作成

---
+++ src/components/ui/QuestionAccordion/QuestionAccordion.tsx
@@ -0,0 +1,37 @@
+import {
+  Accordion as ChakraAccordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  AccordionProps,
+  Box,
+} from '@chakra-ui/react'
+import { QUESTIONS_CONTENTS } from '@src/constants/questions'
+
+type Props = {
+  items: typeof QUESTIONS_CONTENTS
+} & AccordionProps
+
+export const QuestionAccordion = ({ items, ...props }: Props) => {
+  return (
+    <ChakraAccordion allowToggle {...props}>
+      {items.map((item, index) => (
+        <AccordionItem key={`accordion_item_${index}`}>
+          <AccordionButton
+            _expanded={{ bg: 'teal.400', color: 'white' }}
+            py={4}
+          >
+            <Box as={'span'} flex={'1'} textAlign={'left'}>
+              {item.question}
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel bg={'gray.50'} py={8}>
+            ・{item.answer}
+          </AccordionPanel>
+        </AccordionItem>
+      ))}
+    </ChakraAccordion>
+  )
+}
