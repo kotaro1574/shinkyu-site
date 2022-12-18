84b624f8af725b2cbed243ff4f7fff0ace8c7264
kotaro1574 <shake1574@gmail.com>
2022-12-18 15:59:03 +0900

    fix: lintエラー修正

--- src/pages/content.tsx
+++ src/pages/content.tsx
@@ -2,8 +2,8 @@ import { AspectRatio, Box } from '@chakra-ui/react'
 import { AboutSection } from '@src/components/page/home/AboutSection/AboutSection'
 import { ConsultationHoursSection } from '@src/components/page/home/ConsultationHoursSection/ConsultationHoursSection'
 import { IntroductionSection } from '@src/components/page/home/IntroductionSection/IntroductionSection'
-import { TreatmentSection } from '@src/components/page/home/TreatmentSection/TreatmentSection'
 import { QuestionSection } from '@src/components/page/home/QuestionSection/QuestionSection'
+import { TreatmentSection } from '@src/components/page/home/TreatmentSection/TreatmentSection'
 import { Image } from '@src/components/ui/Image'
 
 export const HomeContent = () => {
