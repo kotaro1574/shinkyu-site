bef5cf27fa74244bd1b5a83eaa1195086b8ccf3e
kotaro1574 <shake1574@gmail.com>
2022-12-25 14:17:44 +0900

    create: mainVisualで使用する画像の配列を定義

---
+++ src/constants/mainVisual/index.ts
@@ -0,0 +1,8 @@
+import { staticPath } from '@src/lib/$path'
+
+export const MAIN_VISUAL_SLIDE_IMAGES = [
+  staticPath.images.MainVisual.slide_image_1_jpg,
+  staticPath.images.MainVisual.slide_image_2_jpg,
+  staticPath.images.MainVisual.slide_image_3_jpg,
+  staticPath.images.MainVisual.slide_image_4_jpg,
+]
