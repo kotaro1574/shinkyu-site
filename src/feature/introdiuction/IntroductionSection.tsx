c964bfef1abb30e52b8d3861047ce12c5f9f8306
kotaro1574 <shake1574@gmail.com>
2022-12-30 13:53:00 +0900

    update: IntroductionSectionのレスポンシブ

--- src/feature/introdiuction/IntroductionSection.tsx
+++ src/feature/introdiuction/IntroductionSection.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Box, Center, Container } from '@chakra-ui/react'
+import { AspectRatio, Box, Container, Flex } from '@chakra-ui/react'
 import { Image } from '@src/components/ui/Image'
 import { staticPath } from '@src/lib/$path'
 
@@ -6,18 +6,22 @@ export const IntroductionSection = () => {
   return (
     <Box as={'section'} bg={'#f4fbfc'}>
       <Container py={10}>
-        <Center alignItems={'start'} gap={4}>
-          <Box lineHeight={1.7} maxW={'300px'}>
-            繁内鍼灸治療院は広島県東広島市にある治療院です。鍼灸・整体・産後骨盤ケア・吸い玉を柱とし、それらを適切に組み合わせた治療を提供しております。お身体の悩み、鍼灸に関してのご相談、ご予約、お気軽にご相談ください。
+        <Flex
+          alignItems={{ base: 'center', md: 'start' }}
+          flexDirection={{ base: 'column', md: 'row' }}
+          gap={4}
+          justifyContent={'center'}
+        >
+          <Box lineHeight={1.7} maxW={'500px'} order={{ base: 2, md: 1 }}>
+            繁内鍼灸治療院は広島県東広島市にある治療院です。鍼灸・整体・産後骨盤ケア・吸い玉を柱とし、それらを適切に組み合わせた治療を提供しております。お身体の悩み、鍼灸に関してのご相談、ご予約、お気軽にご相談ください。繁内鍼灸治療院は広島県東広島市にある治療院です。鍼灸・整体・産後骨盤ケア・吸い玉を柱とし、それらを適切に組み合わせた治療を提供しております。お身体の悩み、鍼灸に関してのご相談、ご予約、お気軽にご相談ください。
           </Box>
-          <Box>
-            <AspectRatio
-              h={'200px'}
-              overflow={'hidden'}
-              ratio={4 / 3}
-              rounded={4}
-              w={'300px'}
-            >
+          <Box
+            flexShrink={0}
+            maxW={{ base: '300px', md: '400px' }}
+            order={{ base: 1, md: 2 }}
+            w={'100%'}
+          >
+            <AspectRatio overflow={'hidden'} ratio={4 / 3} rounded={2}>
               <Image
                 alt={'director_image'}
                 layout={'fill'}
@@ -28,7 +32,7 @@ export const IntroductionSection = () => {
               院長 繁内俊輝
             </Box>
           </Box>
-        </Center>
+        </Flex>
       </Container>
     </Box>
   )
