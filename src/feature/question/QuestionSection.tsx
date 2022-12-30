407c34f8e7a3e59288f86c17bd435e2f9e96b59e
kotaro1574 <shake1574@gmail.com>
2022-12-30 17:14:17 +0900

    update: QuestionSectionのstyle調整

--- src/feature/question/QuestionSection.tsx
+++ src/feature/question/QuestionSection.tsx
@@ -5,7 +5,12 @@ import { QuestionAccordion } from '@src/feature/question/QuestionAccordion'
 export const QuestionSection = () => {
   return (
     <Container py={{ base: 5, md: 10 }}>
-      <Heading textAlign={'center'}>よくある質問</Heading>
+      <Heading
+        fontSize={{ base: '4xl', md: '7xl', sm: '5xl' }}
+        textAlign={'center'}
+      >
+        よくある質問
+      </Heading>
       <QuestionAccordion items={QUESTIONS_CONTENTS} mt={8} />
     </Container>
   )
