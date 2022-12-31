0369689ac605b3b4d4f9fcef5db7174a6a12b40d
nyny-sugar <nyny.sugar@gmail.com>
2022-12-31 14:13:35 +0900

    scroll量を取得するよう設定

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,12 +1,34 @@
 import { Box, BoxProps, Container, Flex, Grid } from '@chakra-ui/react'
 import { Button } from '@src/components/ui/Button/Button'
 import { Drawer } from '@src/components/ui/Drawer/Drawer'
+import { useOverViewHeightContext } from '@src/provider/overViewHeight'
+import { useEffect, useState } from 'react'
 
 type Props = BoxProps
 
 export const Header = ({ ...props }: Props) => {
+  const { overViewHeight } = useOverViewHeightContext()
+
+  const [isVisible, setIsVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    window.scrollY > overViewHeight ? setIsVisible(true) : setIsVisible(false)
+  }
+  console.log(isVisible)
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+    return () => window.removeEventListener('scroll', toggleVisibility)
+  }, [])
+
   return (
-    <Box as={'header'} color={'white'} fontSize={'lg'} py={2} {...props}>
+    <Box
+      as={'header'}
+      color={isVisible ? 'black' : 'white'}
+      fontSize={'lg'}
+      py={2}
+      {...props}
+    >
       <Container>
         <Grid alignItems={'center'} gap={4} templateColumns={'1fr auto'}>
           <Box fontSize={{ base: '4xl', lg: '3xl' }}>繁内鍼灸治療院</Box>
