3c26d78e23ebdc00097a5ae6204dcb0d50a956b4
kotaro1574 <shake1574@gmail.com>
2023-01-03 11:45:25 +0900

    update: 料金の文言修正

--- src/feature/treatment/TreatmentSection.tsx
+++ src/feature/treatment/TreatmentSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Grid } from '@chakra-ui/react'
+import { Box, Center, chakra, Divider, Grid, Text } from '@chakra-ui/react'
 import { SectionContainer } from '@src/components/ui/SectionContainer/SectionContainer'
 import { SectionHeading } from '@src/components/ui/SectionHeading/SectionHeading'
 import { TREATMENT_DETAIL_CONTENTS } from '@src/constants/treatments'
@@ -26,10 +26,25 @@ export const TreatmentSection = () => {
             患者さまの症状や現在の状態やご希望をしっかり問診、確認させていただきます。
             下記の施術方法より一人一人に合わせた施術を選択し、相談しながら施術していきます。
           </Box>
-          <Box>1回 4,000 円(税込)</Box>
+          <Box>
+            <Text fontWeight={'medium'}>
+              1回
+              <chakra.span fontSize={'6xl'} fontWeight={'bold'} ml={4}>
+                4,000
+              </chakra.span>
+              円(税込)
+            </Text>
+            <Divider
+              borderColor={'black'}
+              maxW={'210px'}
+              mx={'auto'}
+              my={0}
+              opacity={1}
+            />
+          </Box>
         </Grid>
       </Center>
-      <Grid gap={{ base: 10, md: 20 }} pt={10}>
+      <Grid gap={{ base: 10, md: 20 }} pt={{ base: 10, md: 20 }}>
         {TREATMENT_DETAIL_CONTENTS.map((treatment, index) => (
           <TreatmentDetail
             description={treatment.description}
