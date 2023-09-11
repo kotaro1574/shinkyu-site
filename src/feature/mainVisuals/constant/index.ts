a292197a615b37e2b287a2af745413d1b62cfe58
kotaro1574 <shake1574@gmail.com>
2023-09-11 13:57:39 +0900

    💄: 画像追加＆差し替え

--- src/feature/mainVisuals/constant/index.ts
+++ src/feature/mainVisuals/constant/index.ts
@@ -1,8 +1,8 @@
 import { staticPath } from '@src/lib/$path'
 
 export const MAIN_VISUAL_SLIDE_IMAGES = [
-  staticPath.images.mainVisual.slide_image_1_jpg,
-  staticPath.images.mainVisual.slide_image_2_jpg,
-  staticPath.images.mainVisual.slide_image_3_jpg,
-  staticPath.images.mainVisual.slide_image_4_jpg,
+  staticPath.images.mainVisual.kanban_jpg,
+  staticPath.images.mainVisual.hari_3_jpg,
+  staticPath.images.mainVisual.okyu_2_jpg,
+  staticPath.images.mainVisual.suidama_1_jpg,
 ]
