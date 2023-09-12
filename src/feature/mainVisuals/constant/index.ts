f7cb3345a6b9d7e022771f154397f8f8272fd4ab
kotaro1574 <shake1574@gmail.com>
2023-09-12 16:36:24 +0900

    🔥: 不要な画像削除

--- src/feature/mainVisuals/constant/index.ts
+++ src/feature/mainVisuals/constant/index.ts
@@ -1,7 +1,6 @@
 import { staticPath } from '@src/lib/$path'
 
 export const MAIN_VISUAL_SLIDE_IMAGES = [
-  staticPath.images.mainVisual.kanban_jpg,
   staticPath.images.mainVisual.hari_3_jpg,
   staticPath.images.mainVisual.okyu_2_jpg,
   staticPath.images.mainVisual.suidama_1_jpg,
