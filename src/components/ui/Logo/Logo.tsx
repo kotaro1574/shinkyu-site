9fb8602b89f0505a6de30763f43d36e5860b67a0
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-10-01 12:37:02 +0000

    🎨: Logoにpropsを渡せるようにしたお

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,10 +1,10 @@
-import { AspectRatio } from '@chakra-ui/react'
+import { AspectRatio, AspectRatioProps } from '@chakra-ui/react'
 import { Image } from '@src/components/ui/Image'
 import { staticPath } from '@src/lib/$path'
 
-export const Logo = () => {
+export const Logo = ({ ...props }: AspectRatioProps) => {
   return (
-    <AspectRatio ratio={155 / 30} w={{ base: '255px', md: '355px' }}>
+    <AspectRatio ratio={155 / 30} w={{ base: '255px', md: '355px' }} {...props}>
       <Image
         alt={'logo_image'}
         layout={'fill'}
