e102f9033eadad5e071e647cf36bb0b9dd524379
kotaro1574 <63211616+kotaro1574@users.noreply.github.com>
2023-10-04 20:32:31 +0000

    update: 当院についてのセクションを調整

--- src/feature/introduction/component/IntroductionSection.tsx
+++ src/feature/introduction/component/IntroductionSection.tsx
@@ -1,8 +1,19 @@
-import { AspectRatio, Box, BoxProps, Flex } from '@chakra-ui/react'
+import {
+  AspectRatio,
+  Box,
+  BoxProps,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Link,
+  Text,
+} from '@chakra-ui/react'
 import { Image } from '@src/components/ui/Image'
 import { SectionContainer } from '@src/components/ui/SectionContainer/SectionContainer'
 import { SectionHeading } from '@src/components/ui/SectionHeading/SectionHeading'
 import { staticPath } from '@src/lib/$path'
+import { BsInstagram } from 'react-icons/bs'
 
 type Props = BoxProps
 
@@ -14,45 +25,68 @@ export const IntroductionSection = ({ ...props }: Props) => {
         <Flex
           alignItems={{ base: 'center', md: 'start' }}
           flexDirection={{ base: 'column', md: 'row' }}
-          gap={4}
+          gap={{ base: 4, md: 8 }}
           justifyContent={'center'}
           mt={8}
         >
-          <Box
-            fontSize={{ base: 'md', md: 'lg' }}
-            lineHeight={1.8}
-            maxW={'500px'}
-            order={{ base: 2, md: 1 }}
-          >
-            繁内鍼灸治療院は広島県東広島市にある治療院です。鍼灸・整体・産後骨盤ケア・吸い玉を柱とし、それらを適切に組み合わせた治療を提供しております。お身体の悩み、鍼灸に関してのご相談、ご予約、お気軽にご相談ください。繁内鍼灸治療院は広島県東広島市にある治療院です。鍼灸・整体・産後骨盤ケア・吸い玉を柱とし、それらを適切に組み合わせた治療を提供しております。お身体の悩み、鍼灸に関してのご相談、ご予約、お気軽にご相談ください。
-          </Box>
-          <Box
+          <AspectRatio
             flexShrink={0}
-            maxW={{ base: '500px', md: '400px' }}
-            order={{ base: 1, md: 2 }}
+            maxW={{ base: '400px', md: '500px' }}
+            overflow={'hidden'}
+            ratio={{ base: 5 / 3, md: 4 / 3 }}
+            rounded={4}
             w={'100%'}
           >
-            <AspectRatio
-              overflow={'hidden'}
-              ratio={{ base: 5 / 3, md: 4 / 3 }}
-              rounded={4}
-            >
-              <Image
-                alt={'director_image'}
-                layout={'fill'}
-                objectFit={'cover'}
-                src={staticPath.images.director_image_jpg}
-              />
-            </AspectRatio>
-            <Box
-              fontSize={{ base: 'md', md: 'lg' }}
-              fontWeight={'medium'}
-              mt={1}
-              textAlign={'end'}
-            >
-              院長 繁内俊輝
-            </Box>
-          </Box>
+            <Image
+              alt={'director_image'}
+              layout={'fill'}
+              objectFit={'cover'}
+              src={staticPath.images.director_image_jpg}
+            />
+          </AspectRatio>
+          <Grid gap={4} maxW={'500px'}>
+            <Heading fontSize={{ base: '2xl', md: '5xl', sm: '3xl' }}>
+              伝統と現代の技術、最良の治療をあなたへ
+            </Heading>
+            <Grid fontSize={{ base: 'md', md: 'lg' }} gap={4} lineHeight={1.8}>
+              <Text>
+                当院は、広島県東広島市に位置し、鍼灸、整体、産後骨盤矯正、吸い玉などの治療を組み合わせて、あなたの体調や症状に合わせたケアを提供しています。
+              </Text>
+              <Text>
+                当院では、痛みや不調の原因をしっかりと捉え、より効果的な治療を目指します。治療の疑問や不安な点、ご相談はお気軽にどうぞ。
+              </Text>
+              <Text>
+                また、日々の活動や治療情報はInstagramでも更新中。ぜひチェックしてください。
+              </Text>
+            </Grid>
+            <Flex alignItems={'center'} justifyContent={'space-between'}>
+              <Link
+                isExternal
+                href={'https://www.instagram.com/toshiki_harikyuu/?img_index=3'}
+              >
+                <Button
+                  _hover={{ filter: 'brightness(0.7)' }}
+                  backgroundImage={
+                    'linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)'
+                  }
+                  borderColor={'white'}
+                  borderWidth={'1px'}
+                  colorScheme={'white'}
+                  leftIcon={<BsInstagram fontSize={'22px'} />}
+                >
+                  Instagram
+                </Button>
+              </Link>
+              <Box
+                fontSize={{ base: 'md', md: 'lg' }}
+                fontWeight={'medium'}
+                mt={1}
+                textAlign={'end'}
+              >
+                院長 繁内俊輝
+              </Box>
+            </Flex>
+          </Grid>
         </Flex>
       </SectionContainer>
     </Box>
