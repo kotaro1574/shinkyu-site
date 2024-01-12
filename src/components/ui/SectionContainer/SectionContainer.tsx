035f6b3079f03221ead43d7d0b843fe49602840e
kotaro1574 <shake1574@gmail.com>
2024-01-12 12:19:56 -0800

    fix: maxWを設定

--- src/components/ui/SectionContainer/SectionContainer.tsx
+++ src/components/ui/SectionContainer/SectionContainer.tsx
@@ -4,6 +4,7 @@ export const SectionContainer = ({ children, ...props }: BoxProps) => {
   return (
     <ChakraContainer
       as={'section'}
+      maxW={1000}
       pb={{ base: 16, md: '100px' }}
       pt={{ base: 16, md: '100px' }}
       {...props}
