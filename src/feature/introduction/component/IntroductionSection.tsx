5e9fc965ffafc82574b350352cfaa4191ee0de0f
nyny-sugar <nyny.sugar@gmail.com>
2024-03-27 17:30:51 -0700

    fix:当院について

--- src/feature/introduction/component/IntroductionSection.tsx
+++ src/feature/introduction/component/IntroductionSection.tsx
@@ -3,7 +3,6 @@ import {
   Box,
   BoxProps,
   Flex,
-  Grid,
   Heading,
   Text,
 } from '@chakra-ui/react'
@@ -18,7 +17,7 @@ type Props = BoxProps
 export const IntroductionSection = ({ ...props }: Props) => {
   return (
     <Box bg={'telegram.50'} {...props}>
-      <SectionContainer pb={'50px'} pt={'50px'}>
+      <SectionContainer pb={'100px'} pt={'50px'}>
         <SectionHeading>当院について</SectionHeading>
         <Flex
           alignItems={{ base: 'center', md: 'start' }}
@@ -42,33 +41,34 @@ export const IntroductionSection = ({ ...props }: Props) => {
               src={staticPath.images.director_image_jpg}
             />
           </AspectRatio>
-          <Grid gap={4} maxW={'500px'}>
-            <Heading fontSize={{ base: '2xl', md: '5xl', sm: '3xl' }}>
+          <Box maxW={'500px'}>
+            <Heading
+              as={'h3'}
+              fontSize={{ base: '2xl', md: '5xl' }}
+              lineHeight={1.2}
+              mb={5}
+            >
               伝統と現代の技術、最良の治療をあなたへ
             </Heading>
-            <Grid fontSize={{ base: 'md', md: 'lg' }} gap={4} lineHeight={1.8}>
-              <Text>
-                当院は、広島県東広島市に位置し、鍼灸、整体、産後骨盤矯正、吸い玉などの治療を組み合わせて、あなたの体調や症状に合わせたケアを提供しています。
-              </Text>
-              <Text>
-                当院では、痛みや不調の原因をしっかりと捉え、より効果的な治療を目指します。治療の疑問や不安な点、ご相談はお気軽にどうぞ。
-              </Text>
-              <Text>
-                また、日々の活動や治療情報はInstagramでも更新中。ぜひチェックしてください。
-              </Text>
-            </Grid>
+            <Text lineHeight={1.8} mb={4}>
+              当院は、広島県東広島市に位置し、鍼灸、整体、産後骨盤矯正、吸い玉などの治療を組み合わせて、あなたの体調や症状に合わせたケアを提供しています。
+              <br />
+              当院では、痛みや不調の原因をしっかりと捉え、より効果的な治療を目指します。治療の疑問や不安な点、ご相談はお気軽にどうぞ。
+              <br />
+              また、日々の活動や治療情報はInstagramでも更新中。ぜひチェックしてください。
+            </Text>
             <Flex alignItems={'center'} justifyContent={'space-between'}>
               <InstagramButton />
-              <Box
+              <Text
                 fontSize={{ base: 'md', md: 'lg' }}
                 fontWeight={'medium'}
                 mt={1}
                 textAlign={'end'}
               >
                 院長 繁内俊輝
-              </Box>
+              </Text>
             </Flex>
-          </Grid>
+          </Box>
         </Flex>
       </SectionContainer>
     </Box>
