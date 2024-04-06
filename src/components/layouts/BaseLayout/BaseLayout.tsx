a6035a7de9ffccf9044b0cc82abd28c8771b0f13
nyny-sugar <nyny.sugar@gmail.com>
2024-04-06 15:00:03 -0700

    fix: headerの表示、非表示

--- src/components/layouts/BaseLayout/BaseLayout.tsx
+++ src/components/layouts/BaseLayout/BaseLayout.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react'
 import { Footer } from '@src/components/layouts/Footer/Footer'
 import { Header } from '@src/components/layouts/Header/Header'
+import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
 type Props = {
@@ -8,11 +9,16 @@ type Props = {
 }
 
 export const BaseLayout = ({ children }: Props) => {
+  const router = useRouter()
+  const isPrivacyPolicyPage = router.pathname === '/privacyPolicy'
+
   return (
     <Box bg={'gray.50'} boxShadow={'base'}>
-      <Box position={'fixed'} w={'100%'} zIndex={10}>
-        <Header px={4} />
-      </Box>
+      {!isPrivacyPolicyPage && (
+        <Box position={'fixed'} w={'100%'} zIndex={10}>
+          <Header px={4} />
+        </Box>
+      )}
       {children}
       <Footer />
     </Box>
