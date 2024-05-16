5f07afc872122c603ccba4d11c5120ca48485d49
nyny-sugar <nyny.sugar@gmail.com>
2024-05-16 12:53:45 -0700

    next.jsとchakra-uiのバージョンアップ

--- src/feature/price/component/PriceSection.tsx
+++ src/feature/price/component/PriceSection.tsx
@@ -1,4 +1,4 @@
-import { BoxProps } from '@chakra-ui/react'
+import { BoxProps, Card, Text } from '@chakra-ui/react'
 import { SectionContainer } from '@src/components/ui/SectionContainer/SectionContainer'
 import { SectionHeading } from '@src/components/ui/SectionHeading/SectionHeading'
 
@@ -6,6 +6,14 @@ export const PriceSection = (props: BoxProps) => {
   return (
     <SectionContainer pt={{ base: '64px', md: '100px' }} {...props}>
       <SectionHeading>料金表</SectionHeading>
+      <Card mt={'32px'} p={'16px'}>
+        <Text fontSize={'24px'} fontWeight={'bold'} mb={'16px'}>
+          料金表
+        </Text>
+        <Text fontSize={'16px'} mb={'16px'}>
+          ここに料金表を記載する
+        </Text>
+      </Card>
     </SectionContainer>
   )
 }
