40c4f7248a3ff955eb4b56ef1ddf45199c049f17
nyny-sugar <nyny.sugar@gmail.com>
2024-05-30 14:13:39 -0700

    Card追加

--- src/feature/price/component/PriceSection.tsx
+++ src/feature/price/component/PriceSection.tsx
@@ -1,4 +1,15 @@
-import { BoxProps, Card, Text } from '@chakra-ui/react'
+import {
+  Box,
+  BoxProps,
+  Card,
+  CardBody,
+  CardFooter,
+  Flex,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+} from '@chakra-ui/react'
 import { SectionContainer } from '@src/components/ui/SectionContainer/SectionContainer'
 import { SectionHeading } from '@src/components/ui/SectionHeading/SectionHeading'
 
@@ -6,13 +17,127 @@ export const PriceSection = (props: BoxProps) => {
   return (
     <SectionContainer pt={{ base: '64px', md: '100px' }} {...props}>
       <SectionHeading>料金表</SectionHeading>
-      <Card mt={'32px'} p={'16px'}>
-        <Text fontSize={'24px'} fontWeight={'bold'} mb={'16px'}>
-          料金表
-        </Text>
-        <Text fontSize={'16px'} mb={'16px'}>
-          ここに料金表を記載する
-        </Text>
+      <Card p={{ base: '0', md: '16px' }}>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing={'4'}>
+            <Box>
+              <Heading
+                fontSize={{ base: '15px', md: '18px' }}
+                textTransform={'uppercase'}
+              >
+                骨格&神経&筋膜整体（全身）
+              </Heading>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Box>
+                  <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                    鍼灸・整体併用
+                  </Text>
+                  <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                    整体のみ
+                  </Text>
+                </Box>
+                <Text fontSize={{ base: '14px', md: '16px' }}>
+                  90分 12,000円
+                </Text>
+              </Flex>
+            </Box>
+            <Box>
+              <Heading
+                fontSize={{ base: '15px', md: '18px' }}
+                textTransform={'uppercase'}
+              >
+                複数の症状改善・再発予防特化施術（2〜3部位）
+              </Heading>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Box>
+                  <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                    鍼灸・整体併用
+                  </Text>
+                  <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                    整体のみ
+                  </Text>
+                </Box>
+                <Text fontSize={{ base: '14px', md: '16px' }}>
+                  60分 6,500円
+                </Text>
+              </Flex>
+            </Box>
+            <Box>
+              <Heading
+                fontSize={{ base: '15px', md: '18px' }}
+                textTransform={'uppercase'}
+              >
+                痛み・痺れ特化施術（1部位）
+              </Heading>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Box>
+                  <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                    鍼灸・整体併用
+                  </Text>
+                  <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                    整体のみ
+                  </Text>
+                </Box>
+                <Text fontSize={{ base: '14px', md: '16px' }}>
+                  40分 5,000円
+                </Text>
+              </Flex>
+            </Box>
+            <Box>
+              <Heading
+                fontSize={{ base: '15px', md: '18px' }}
+                textTransform={'uppercase'}
+              >
+                オプション（予約時にお伝えください）
+              </Heading>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  吸い玉（カッピング）
+                </Text>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  15分 1,500円
+                </Text>
+              </Flex>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  オイルスライドカッピング
+                </Text>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  15分 2,000円
+                </Text>
+              </Flex>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  上記カッピング両方
+                </Text>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  20分 3,000円
+                </Text>
+              </Flex>
+            </Box>
+            <Box>
+              <Heading
+                fontSize={{ base: '15px', md: '18px' }}
+                textTransform={'uppercase'}
+              >
+                初診料・再診料
+              </Heading>
+              <Flex alignItems={'center'} justifyContent={'space-between'}>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  初診の方・半年以上期間が空いた方
+                </Text>
+                <Text fontSize={{ base: '14px', md: '16px' }} pt={'2'}>
+                  1,000円
+                </Text>
+              </Flex>
+            </Box>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <Text fontSize={{ base: 'xs', md: 'sm' }}>
+            ※施術時間は問診、お着替え、施術、お会計の時間も含まれます。
+          </Text>
+        </CardFooter>
       </Card>
     </SectionContainer>
   )
