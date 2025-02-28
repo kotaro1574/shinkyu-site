651d21997703b6fc63a8fb968d90befb323e3573
nyny-sugar <nyny.sugar@gmail.com>
2025-02-28 17:19:05 +0900

    言語を英語から日本語に

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang={'en'}>
+    <html lang={'ja'}>
       <body>
         <ChakraProvider theme={theme}>
           <OverviewHeightProvider>{children}</OverviewHeightProvider>
